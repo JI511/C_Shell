53183064a4395bb88e791b1b3d1b707ac12ad239
John Ingwersen <johni@iastate.edu>
2017-10-17 01:16:47 -0500

    more shell progress

--- shell.c
+++ shell.c
@@ -10,6 +10,8 @@
 #include <sys/wait.h>
 
 void setupCommand(char* command);
+char* removeSpace(char* string);
+int executeCommand(int returnValue, int isFirst, int test);
 
 char *args[256];
 pid_t pid;
@@ -32,7 +34,19 @@ int main(int argc, char const *argv[]) {
 
         while (pipe != NULL){ //While there exists another pipe.
             *pipe = '\0'; //set the current location to null terminator.
-
+            setupCommand(command);
+            int returnValue = 0; //will be used for previous command value, 0 initially.
+            int isFirst = 1; //check on if command is first in sequence (piping).
+            //check if any args
+            if (args[0] != NULL){
+                if (strcmp(args[0], "exit") == 0){
+                    //if exit is typed.
+                    exit(0);
+                }
+                returnValue = executeCommand(returnValue, isFirst, 0);
+            } else {
+                returnValue = 0;
+            }
 
 
         }
@@ -41,6 +55,7 @@ int main(int argc, char const *argv[]) {
     return 0;
 }
 
+//iterate through string removing any white space
 char* removeSpace(char* string){
     while (isspace(*string)){
         ++string;
@@ -50,6 +65,29 @@ char* removeSpace(char* string){
 
 void setupCommand(char* command){
     command = removeSpace(command); //get rid of any leading white space
+    char* space = strchr(command, ' ');
+    int count = 0;
+    while (space != NULL) { //While there exists another space
+        space[0] = '\0';
+        args[count] = command;
+        count++;
+        command = removeSpace(space + 1); //get new command (next param)
+        space = strchr(command, ' '); //reset next space
+    }
+    printf("%c", *command);
+    if (command[0] != '\0'){
+        args[count] = command;
+        space = strchr(command, '\n');
+        space[0] = '\0';
+        count++;
+    }
 
+    // last argument must be set to NULL.
+    args[count] = NULL;
+
+}
+
+int executeCommand(int returnValue, int isFirst, int test){
+    //how to handle pipe?
 
 }
