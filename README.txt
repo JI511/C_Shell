2ee355c829ab57b6adcbfb5f22c6f46797e78825
John Ingwersen <johni@iastate.edu>
2017-10-19 10:21:39 -0500

    finished shell

---
+++ README.txt
@@ -0,0 +1,7 @@
+Run make or make shell to compile
+use ./shell to execute
+
+Notes:
+When using !! the command executed is added to history. For example if !! is executed and ls was the last command then ls is added to history.
+History is limited to 100.
+History is added to history when run.
--- shell.c
+++ shell.c
@@ -13,17 +13,34 @@ void setupCommand(char* command);
 char* removeSpace(char* string);
 int executeCommand(int returnValue, int isFirst, int test);
 void clean(int i);
+void showHistory();
+char* peekFront();
+char* peekRear();
+int isEmpty();
+int isFull();
+int size();
+void insert(char* value);
+char* getElement(int index);
+
 
 char *args[256];
 pid_t pid;
 char str[256];
 int count = 0;
 
+//queue values
+#define max 100
+
+char* array[max];
+int front = 0;
+int rear = -1;
+int historyCount = 0;
+
 int main(int argc, char const *argv[]) {
     printf("C Shell. Type 'exit' to quit.\n");
     while (1){
-        printf("$> ");
-        fflush(NULL); //print prompt and flush everything out.
+        printf("osh> ");
+        fflush(stdout); //print prompt and flush everything out.
 
         int returnValue = 0; //will be used for previous command value, 0 initially.
         int isFirst = 1; //check if command is first in sequence (piping).
@@ -34,11 +51,49 @@ int main(int argc, char const *argv[]) {
             //maybe add message or rerun loop?
         }
         char* command = str; //entire command
+
+
+
+        char* tmp = strdup(command);
+        tmp[strlen(tmp)-1] = 0;
+        if (strcmp(tmp, "!!") == 0){
+            command = strdup(array[rear]);
+            command[strlen(command)+1] = '\0';
+            command[strlen(command)] = '\n';
+            char* tmp1 = strdup(command);
+            tmp1[strlen(tmp1)-1] = 0;
+            insert(strdup(tmp1));
+        }
+        else if (tmp[0] == '!'){
+            //printf("yes\n");
+            int z = 1;
+            char y[3];
+            while (isdigit(tmp[z]) && z < 4){
+                y[z-1] = tmp[z];
+                z++;
+            }
+            int num = atoi(y);
+            //printf("num %d\n", num);
+            command = strdup(getElement(num));
+            //printf("%s\n", command);
+            command[strlen(command)+1] = '\0';
+            command[strlen(command)] = '\n';
+            char* tmp1 = strdup(command);
+            tmp1[strlen(tmp1)-1] = 0;
+            insert(strdup(tmp1));
+        }
+        else {
+            insert(tmp);
+        }
+
+
         //char *strchr(const char *str, int c) get location of pipe
         char* pipe = strchr(command, '|'); //returns pointer to location of pipe. NULL if none.
 
         while (pipe != NULL){ //While there exists another pipe.
             *pipe = '\0'; //set the current location to null terminator.
+            //printf("command: '%s'\n", command);
+            //addHistory(command);
             setupCommand(command);
 
             //check if any args
@@ -58,7 +113,10 @@ int main(int argc, char const *argv[]) {
         }
 
         //deal with last command (or only command if no piping).
+        //printf("command: '%s'\n", command);
+        //addHistory(command);
         setupCommand(command);
+        //printf("test\n");
         if (args[0] != NULL){
             if (strcmp(args[0], "exit") == 0){
                 exit(0);
@@ -69,9 +127,79 @@ int main(int argc, char const *argv[]) {
         }
         clean(count); //make sure all processes are terminated.
         count = 0;
-        fflush(NULL);
     }
     return 0;
+}
+
+void showHistory(){
+    if (isEmpty()) {
+        printf("No commands in history.");
+        return;
+    }
+    int i = rear;
+    int k = 0;
+    while (i >= 0 && k < 10){
+        printf("%d %s\n", i+1, array[i]);
+        i--;
+        k++;
+    }
+
+}
+
+char* peekFront(){
+    return array[front];
+}
+char* peekRear(){
+    return array[rear];
+}
+
+
+int isEmpty(){
+    if (historyCount == 0){
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(){
+    if (historyCount == max){
+        return 1;
+    }
+    return 0;
+}
+
+int size(){
+    return historyCount;
+}
+
+void insert(char* value){
+    if (!isFull()){
+        if (rear == max - 1){
+            rear = -1;
+        }
+        array[++rear] = value;
+        historyCount++;
+        //printf("f%d, r%d, value: %s", front, rear, value);
+
+    }
+}
+
+//will remove the first element. be careful when calling as
+//you dont want to remove items from main history (just get their value).
+char* removeElement(char* arr[]){
+    char* value = arr[front++];
+    if (front == max){
+        front = 0;
+    }
+    return value;
+}
+
+char* getElement(int index){
+    if (index-1 <= rear){
+        //printf("in %s\n", array[index-1]);
+        return array[index-1];
+    }
+    return "Invalid index.";
 }
 
 //iterate through string removing any white space
@@ -87,7 +215,6 @@ void setupCommand(char* command){
 
     command = removeSpace(command); //get rid of any leading white space
     char* space = strchr(command, ' '); //find end of command.
-
     while (space != NULL) { //While there exists another space
         space[0] = '\0';
         args[count] = command;
@@ -95,8 +222,10 @@ void setupCommand(char* command){
         command = removeSpace(space + 1); //get new command (next param)
         space = strchr(command, ' '); //reset next space
     }
+
     //printf("COMMAND: %s\n", command);
     if (command[0] != '\0'){
+
         args[count] = command;
         space = strchr(command, '\n');
         space[0] = '\0';
@@ -128,7 +257,7 @@ int executeCommand(int returnValue, int isFirst, int test){
     // int file = open("myfile.txt", O_APPEND | O_WRONLY);
     // f(dup2(file,1) < 0)
 
-
+    //printf("%s\n", args[0]);
     int pipes[2];
     pipe(pipes);
     pid = fork();
@@ -146,10 +275,20 @@ int executeCommand(int returnValue, int isFirst, int test){
         }
 
         //run command
+
+
+
         int run = execvp(args[0], args);
         if (run == -1) {
+            if (strcmp(args[0], "history") == 0){
+                showHistory();
+            } else{
+                printf("Invalid command.\n");
+            }
             _exit(2);
         }
+
+
     } else {
         waitpid(pid, &status, 0); //wait for child to finish!!
     }
